5e389264152c278216ab58269c0331cdcfa9bafb
Mohammed Samir <ms.2052001@gmail.com>
2021-01-17 16:12:40 +0200

    download some VSC themes json file

---
+++ src/themes/index.js
@@ -0,0 +1,21 @@
+import cobalt2 from './cobalt2.json';
+import darcula from './darcula.json';
+import horizon from './horizon.json';
+import nightOwl from './nightOwl.json';
+import nightOwlLight from './nightOwlLight.json';
+import nord from './nord.json';
+import oneDark from './oneDark.json';
+import palenight from './palenight.json';
+import oneMonokai from './oneMonokai.json';
+
+export default {
+  cobalt2,
+  darcula,
+  horizon,
+  nightOwl,
+  nightOwlLight,
+  nord,
+  oneDark,
+  palenight,
+  oneMonokai
+};
